fe87d775d40f99e16790ba56e7ab255ac882c4f1
Chethan <chethansputran222@gmail.com>
2021-07-22 11:32:06 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request,send_file
+from datetime import datetime
+
+app = Flask(__name__) 
+
+@app.route('/maps/GuddeBettaTruckingPlace')
+def GuddeBettaTruckingPlace():
+    return render_template('GuddeBettaTruckingPlace.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+     
+if __name__ == '__main__':
+
+    app.run()
